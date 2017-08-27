ce9e470eef327d2e95426a86fa581bd6c1b50c4d
zak905 <zakaria.amine88@gmail.com>
2017-08-27 19:13:09 +0200

    update gwty-leaflet and GWTversion

--- src/main/java/com/gwidgets/leaflet/client/GwtyLeafletStarter.java
+++ src/main/java/com/gwidgets/leaflet/client/GwtyLeafletStarter.java
@@ -4,25 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.JavaScriptObject;
 import com.google.gwt.core.shared.GWT;
-import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.RootPanel;
 import com.gwidgets.api.leaflet.Circle;
 import com.gwidgets.api.leaflet.L;
 import com.gwidgets.api.leaflet.LatLng;
 import com.gwidgets.api.leaflet.Map;
 import com.gwidgets.api.leaflet.Polygon;
 import com.gwidgets.api.leaflet.Rectangle;
-import com.gwidgets.api.leaflet.elemental.Document;
-import com.gwidgets.api.leaflet.elemental.Function;
-import com.gwidgets.api.leaflet.elemental.HTMLElement;
+import com.gwidgets.api.leaflet.events.Event;
+import com.gwidgets.api.leaflet.events.EventCallback;
 import com.gwidgets.api.leaflet.events.EventTypes;
+import com.gwidgets.api.leaflet.events.MouseEvent;
+import com.gwidgets.api.leaflet.options.CircleOptions;
 import com.gwidgets.api.leaflet.options.MapOptions;
 import com.gwidgets.api.leaflet.options.PathOptions;
 import com.gwidgets.api.leaflet.options.PolylineOptions;
 import com.gwidgets.api.leaflet.options.PopupOptions;
-import com.gwidgets.api.leaflet.options.TooltipOptions;
+import com.gwidgets.api.leaflet.options.TileLayerOptions;
 import com.gwidgets.api.leaflet.utils.LeafletResources;
 
 /**
@@ -37,51 +35,55 @@ public class GwtyLeafletStarter implements EntryPoint {
 
 	@Override
 	public void onModuleLoad() {
-		
-		
-		LeafletResources.whenReady(
+		//true for unminified, false for minified
+		LeafletResources.whenReady(true, 
 				e -> 
-		             {
-		            	 
+		             {	 
 		//Map options required values: center point, zoom, and a min zoom 
-	    MapOptions options = new MapOptions.Builder(L.latLng(52.51, 13.40), 12, 12).dragging(false).build();
+	    MapOptions options = new MapOptions.Builder(L.latLng(52.485611, 13.416460), 11.0, 10.0)
+	    		                       .maxZoom(400.0).build();
 		
 		final Map map = L.map("map", options);
-		
-		
-		GWT.log(options.getZoom()+"");
+			
+		//TileLayerOptions tloptions = new TileLayerOptions.Builder().minZoom(1).maxZoom(17).build();
+		L.tileLayer(MAP_URL, null).addTo(map);	
 
-		L.tileLayer(MAP_URL, null).addTo(map);
-		
-
-		map.on(EventTypes.Map.CLICK, new Function() {
+		map.on(EventTypes.MapEvents.CLICK, new EventCallback<MouseEvent>() {
 			@Override
-			public JavaScriptObject call(JavaScriptObject event) {
-				
+			public void call(MouseEvent event) {
+				GWT.log("Mouse event "+event.getLayerPoint().x);
 				
 				if (firstClickFlag) {
 					PopupOptions options = new PopupOptions.Builder()
-							               .zoomAnimation(false)
 							               .build();
 					map.openPopup("Hello, Welcome to Berlin!", L.latLng(52.51, 13.40), options);
 					firstClickFlag = false;
 				}
 
-				return null;
+			}
+		});
+		
+		map.on(EventTypes.MapEvents.ZOOMSTART, new EventCallback<Event>() {
+			@Override
+			public void call(Event event) {
+				// TODO Auto-generated method stub
+				GWT.log(map.getZoom()+"");
 			}
 		});
 
-		PathOptions circleOptions = new PathOptions.Builder()
+		CircleOptions circleOptions = new CircleOptions.Builder()
 				                    .fillColor("#b35d20")
 				                    .color("#f54e02")
+				                    .radius(500)
 				                     .build();
 		
 		//creating a circle as Leaflet Js: L.circle
 
-		Circle circle = L.circle(L.latLng(52.53, 13.33), 1000, circleOptions);
+		Circle circle = L.circle(L.latLng(52.53, 13.33), circleOptions);
 		circle.addTo(map);
+		//circle.setRadius(500);
 		
-		
+		GWT.log("radius " + circle.getRadius());	
 
 		circle.bindPopup("This area has cheap rent", null);
 
@@ -110,25 +112,22 @@ public class GwtyLeafletStarter implements EntryPoint {
 		coordinates.add(L.latLng(52.51831005956184, 13.369331359863281));
 
 		LatLng[] coordinatesArray = (LatLng[]) coordinates.toArray();
+		
+		LatLng[][] coordinatesPolygon = {coordinatesArray};
 
 		PolylineOptions polygonOptions = new PolylineOptions.Builder()
 				                         .fillColor("#008000")
 				                         .color("#1d921d")
 				                         .build();
 
-		Polygon tiergartenPolygon = L.polygon(coordinatesArray, polygonOptions);
+		Polygon tiergartenPolygon = L.polygon(coordinatesPolygon, polygonOptions);
 
 		tiergartenPolygon.addTo(map);
 
 		tiergartenPolygon.bindPopup("This is Tiergarten: one of the best parks in Berlin", null);
 		tiergartenPolygon.bindTooltip("park", null);
 		
-		            	 return null;});
-		
-		
-		
-		
-		
+		            	 return null;}
+				);
 	}
-
 }
